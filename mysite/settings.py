636c2fa08e33a3285855f1cbba87ec7694337897
Siddhant Gautam <sgsidgautam@gmail.com>
2023-12-02 23:53:25 -0700

    updated files

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1%8^^#@#q(iqlg5udmbu56)$t9rn!#y0+qa9!rsrdbawew#dr*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
